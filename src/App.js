25d5a513a76080a51f4fb1404d31e3c743bc410b
Radoslav Velichkov <rado88x@gmail.com>
2022-02-08 18:44:25 +0200

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./pages/Products";
 import Welecome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
+import HomePage from "./pages/HomePage";
 import MainHeader from "./components/MainHeader";
 
 function App() {
@@ -10,12 +12,23 @@ function App() {
         <MainHeader></MainHeader>
       </header>
       <main>
-        <Route path="/welcome">
-          <Welecome></Welecome>
-        </Route>
-        <Route path="/products">
-          <Products></Products>
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+           <Redirect to="/Homepage"/>
+          </Route>
+          <Route path="/Homepage">
+          <HomePage></HomePage>
+          </Route>
+          <Route path="/welcome">
+            <Welecome></Welecome>
+          </Route>
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
     return (
-        <h2>Produsts Page</h2>
+        <section>
+        <h2>Products Page</h2>
+        <ul>
+            <li><Link to="/products/p1"> A Book </Link></li>
+            <li> <Link to="/products/p2">A Carpet</Link></li>
+            <li> <Link to="/products/p3">An Online Course</Link></li>
+            <li> <Link to="/products/p4">Conference invitation</Link></li>
+        </ul>
+        </section>
     )
 };
 
