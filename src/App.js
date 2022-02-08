378ac58229c6f80551aca4921e4ce2e476ec439c
Radoslav Velichkov <rado88x@gmail.com>
2022-02-08 15:44:05 +0200

    added NavLink instead Link or a href

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { Route } from "react-router-dom";
 import Products from "./pages/Products";
 import Welecome from "./pages/Welcome";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Route path="/welcome">
-        <Welecome></Welecome>
-      </Route>
-      <Route path="/products">
-        <Products></Products>
-      </Route>
+      <header>
+        <MainHeader></MainHeader>
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welecome></Welecome>
+        </Route>
+        <Route path="/products">
+          <Products></Products>
+        </Route>
+      </main>
     </div>
   );
 }
